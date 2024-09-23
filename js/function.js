5ac08ff9013a7f2b4321a9ac0242f3589ec673c4
fayazsakeer <fayazfaizy619@gmail.com>
2024-09-23 17:55:56 +0530

    fun

---
+++ js/function.js
@@ -0,0 +1,25 @@
+// function ispalindrome(n){
+//    let temp = n
+//    let rev = 0
+    
+//     while(n>0){
+//         let last = n%10;
+//         rev=rev*10+last;
+//         n=(n-last)/10
+//     }
+//     return temp==rev
+// }
+// let x = 1000
+// for(i=0 ;i<x ; i++){
+//   if(ispalindrome(i)) {
+//     console.log(i);
+//   }
+// }
+
+function fib(n){
+    if (n<=1) return n;
+    return fib(n-1)+fib(n-2);
+}
+
+let n = 45
+console.log(fib(n));
