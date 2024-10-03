6d6f302540d8814e854f565bf1907e07ae92ba3d
fayazsakeer <fayazfaizy619@gmail.com>
2024-10-03 17:57:10 +0530

    03/10/2024

---
+++ class/promis.js
@@ -0,0 +1,26 @@
+// let resp = fetch('https://dummyjson.com/products')
+// resp.then(res => {
+//     console.log(resp);
+//     return res.json()
+// })
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(e => {
+//     console.log(e);
+// })
+
+function abcd(name) {
+    let promise = new Promise((res,rej) => {
+        setTimeout(() => res(`Hello ${name}`),2000)
+    })
+    return promise
+}
+
+let resp = abcd ("Alan")
+resp.then((result) => {
+    console.log(result);
+})
+.catch(e => {
+    console.log(e);
+})
